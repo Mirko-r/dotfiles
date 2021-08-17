6902f5210b6d21658b55618002a98e6ec228e22b
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-08-17 11:30:04 +0200

    Add files via upload

---
+++ bash/install.sh
@@ -0,0 +1,22 @@
+
+echo " Installing bat
+A cat(1) clone with syntax highlighting and Git integration......
+"
+
+cargo install bat
+
+echo " Installing TheFuck
+that corrects errors in previous console commands......
+"
+
+pip install thefuk
+
+echo "Building hstr
+Easily view, navigate and search your command history with shell history suggest box for bash....
+"
+
+git clone https://github.com/dvorka/hstr.git
+cd ./build/tarball && ./tarball-automake.sh && cd ../..
+./configure && make && make install
+
+echo "Done!"
